2840e5b2db6f9b9cb1a526541e3db603e6e71925
William Sampaio <56176702+WilliamSampaio@users.noreply.github.com>
2024-08-02 00:58:42 -0400

    login view

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,7 +1,39 @@
 <template>
-    login
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col col-6">
+                <div class="card my-4">
+                    <div class="card-body">
+                        <h5 class="card-title">Login</h5><br>
+                        <div class="row mb-3">
+                            <label for="email-input" class="col-sm-2 col-form-label">Email</label>
+                            <div class="col-sm-10">
+                                <input type="email" class="form-control" id="email-input" name="email" required>
+                            </div>
+                        </div>
+                        <div class="row mb-3">
+                            <label for="passwd-input" class="col-sm-2 col-form-label">Password</label>
+                            <div class="col-sm-10">
+                                <input type="password" class="form-control" id="passwd-input" name="passwd" required>
+                            </div>
+                        </div><br>
+                        <button class="btn btn-primary" @click="login">Sign in</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
-<script></script>
+<script>
+export default {
+    name: 'LoginView',
+    methods: {
+        login() {
+            this.$router.push({ name: 'home' })
+        }
+    }
+}
+</script>
 
 <style scoped></style>
